ea095d37a86c47c0cdbb9127c5169d076fff6240
jacobean.eth <24348787+0xJacobean@users.noreply.github.com>
2022-10-04 11:43:15 -0700

    Update and rename walletchecker005.js to walletchecker006.js

--- walletchecker006.js
+++ walletchecker006.js
@@ -5,9 +5,9 @@ let wallet = [
   "0x5678"
 ]
 
-$( "#check-button" ).click(function(address) {
-  if (wallet.includes(address.toString())) {
-    console.log("clicked here");
+$( "#check-button" ).click(function() {
+  if (wallet.includes($("#address-field").text())) {
+    console.log("clicked here", $("#address-field").text();
     $("#success-message").val(`You are in The Cohort and will be able to mint on ${cohortMintDate}`);
     $( "#form-success" ).show('slow');
   } else {
