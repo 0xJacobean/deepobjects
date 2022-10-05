ed1da3d6c05d8e164e708e040cd7413708771d74
jacobean.eth <24348787+0xJacobean@users.noreply.github.com>
2022-10-04 18:31:53 -0700

    Update and rename walletchecker021.js to walletchecker022.js

--- walletchecker022.js
+++ walletchecker022.js
@@ -7,6 +7,15 @@ let wallet = [
   "0x5678"
 ]
 
+$(document).ready(function() {
+  $(window).keydown(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+});
+
 $( ".exit" ).click(function() {
   $("#form-success").hide()
   $("#form-error").hide()
